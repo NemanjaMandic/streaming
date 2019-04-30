c0ad265b05bffaf9c12d97e61fe9225de8b68872
NemanjaMandic <nemanjans84@gmail.com>
2019-04-30 19:30:20 +0200

    added stream list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import stream from '../apis/stream';
-import {SIGN_IN, SIGN_OUT} from './types';
-import StreamShow from '../components/streams';
+import {
+    SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from './types';
+
 
 export const signIn = (userId) => {
     return {
@@ -15,6 +23,29 @@ export const signOut = () => {
     }
 }
 
+
+
 export const createStream = formValues => async dispatch => {
-  stream.post('/streams', formValues);
+  const response = await stream.post('/streams', formValues);
+  dispatch({type: CREATE_STREAM, payload: response.data})
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await stream.get('/streams');
+    dispatch({type: FETCH_STREAMS, payload: response.data});
+};
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await stream.get(`/streams/${id}`);
+    dispatch({type: FETCH_STREAM, payload: response.data});
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await stream.get(`/streams/${id}`, formValues);
+    dispatch({type: EDIT_STREAM, payload: response.data});
+};
+
+export const deleteStream = (id) => async dispatch => {
+     await stream.delete(`/streams/${id}`);
+    dispatch({type: DELETE_STREAM, payload: id});
 };
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-    return(
-        <div>
-            StreamList
-        </div>
-    );
+class StreamList extends Component {
+   
+    componentDidMount(){
+        this.props.fetchStreams()
+    }
+
+    renderList(){
+        const streams = this.props.streams.map(strim => {
+            return(
+                <div className="item" key={strim.id}>
+                <i className="large middle aligned icon camera" />
+                <div className="content">
+                 <h3>{strim.title}</h3>
+                 <div className="description">
+                    <p>{strim.description}</p>
+                 </div>
+                </div>
+                </div>
+            );
+        });
+        return streams;
+    }
+    render() { 
+        console.log(this.props.streams)
+        return ( 
+            <div>
+              <h1>Lista Strimova</h1>
+             
+                 <div className="ui celled list">
+                 {this.renderList()}
+                 </div>
+             
+            </div>
+         );
+    }
+
+   
+}
+ 
+const mapStateToProps = state => {
+    return { streams: Object.values(state.streams)}
 }
 
-export default StreamList;
+export default connect(mapStateToProps, {fetchStreams})(StreamList);
